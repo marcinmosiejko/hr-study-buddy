7ecc1d61dc6b574b478e1def57f6acfa73250017
Marcin <marcinmosiejko@gmail.com>
2022-09-20 17:54:29 +0200

    Add user deleting functionality

---
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Average from 'components/atoms/average/Average';
+import UserName from 'components/atoms/userName/userName';
+import Button from 'components/atoms/button/Button';
+import { Wrapper } from './UsersListItem.styles';
+
+const UsersListItem = ({
+  deleteUser,
+  userData: { name, attendance, average = '0%' },
+}) => (
+  <Wrapper>
+    <Average average={average} />
+    <UserName name={name} attendance={attendance} />
+    <Button onClick={() => deleteUser(name)} />
+  </Wrapper>
+);
+
+UsersListItem.propTypes = {
+  userData: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    attendance: PropTypes.string.isRequired,
+    average: PropTypes.string,
+  }),
+};
+
+export default UsersListItem;
---
+++ src/components/organisms/UsersList/UsersList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { users } from 'data/users';
+import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
+import { Wrapper } from './UsersList.styles';
+
+class UsersList extends React.Component {
+  state = {
+    users,
+  };
+
+  deleteUser = (name) => {
+    const filteredUsers = this.state.users.filter((user) => user.name !== name);
+    this.setState({ users: filteredUsers });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <ul>
+          {this.state.users.map((userData, i) => (
+            <UsersListItem
+              deleteUser={this.deleteUser}
+              index={i}
+              key={Object.values(userData).join('')}
+              userData={userData}
+            />
+          ))}
+        </ul>
+      </Wrapper>
+    );
+  }
+}
+
+export default UsersList;
