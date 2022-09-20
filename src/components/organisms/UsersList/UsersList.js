39cc87d88657fdd023a4c74d3e686ecaa6e030d3
Marcin <marcinmosiejko@gmail.com>
2022-09-20 22:01:27 +0200

    Add fake data fetching using class component

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -3,10 +3,35 @@ import { users } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper } from './UsersList.styles';
 
+const mockAPI = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (users) {
+        resolve([...users]);
+      } else {
+        reject({ message: 'Error' });
+      }
+    }, 2000);
+  });
+};
+
 class UsersList extends React.Component {
   state = {
-    users,
+    users: null,
+    isLoading: false,
   };
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    mockAPI()
+      .then((data) => this.setState({ users: data, isLoading: false }))
+      .catch((err) => console.error(err.message));
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.isLoading !== this.state.isLoading)
+      console.log('Loading state has changed');
+  }
 
   deleteUser = (name) => {
     const filteredUsers = this.state.users.filter((user) => user.name !== name);
@@ -16,8 +41,9 @@ class UsersList extends React.Component {
   render() {
     return (
       <Wrapper>
+        <h1>{this.state.isLoading ? 'Loading data...' : 'Users list'}</h1>
         <ul>
-          {this.state.users.map((userData, i) => (
+          {this.state.users?.map((userData, i) => (
             <UsersListItem
               deleteUser={this.deleteUser}
               index={i}
