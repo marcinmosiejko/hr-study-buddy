9e7a2cbd8a63908e9dd646a6115a627e7e92371c
Marcin <marcinmosiejko@gmail.com>
2022-09-20 15:40:12 +0200

    Add simple event handling to button

--- src/components/atoms/average/Average.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyledAverage } from './Average.styles';
-
-const Average = ({ average }) => (
-  <StyledAverage average={average}>{average}</StyledAverage>
-);
-
-export default Average;
