2914bde2b122b662fbe8fc5d01ad43d64790993d
Marcin <marcinmosiejko@gmail.com>
2022-09-24 14:56:21 +0200

    Create simple useReducer example

--- src/views/AddUser.js
+++ src/views/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useReducer } from 'react';
 import FormField from 'components/molecules/UsersListItem/FormField/FormField';
 import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
 import { Button } from 'components/atoms/Button/Button';
@@ -12,25 +12,42 @@ const initialFormState = {
   average: '',
 };
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return state + 1;
+
+    case 'SUBTRACT':
+      return state - 1;
+
+    default:
+      return state;
+  }
+  return state;
+};
+
 const AddUser = () => {
-  const [formValues, setFormValues] = useState(initialFormState);
+  const [value, dispatch] = useReducer(reducer, 0);
+  // const [formValues, setFormValues] = useState(initialFormState);
 
   const { handleAddUser } = useContext(UsersContext);
 
   const handleInputChange = (e) => {
-    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+    // setFormValues({ ...formValues, [e.target.name]: e.target.value });
   };
 
   const handleSubmitUser = (e) => {
     e.preventDefault();
-    handleAddUser(formValues);
-    setFormValues(initialFormState);
+    // handleAddUser(formValues);
+    // setFormValues(initialFormState);
   };
 
   return (
     <ViewWrapper as="form" onSubmit={handleSubmitUser}>
-      <Title>Add new student</Title>
-      <FormField
+      <Title>{value}</Title>
+      <button onClick={() => dispatch({ type: 'ADD' })}>Add</button>
+      <button onClick={() => dispatch({ type: 'SUBTRACT' })}>Subtract</button>
+      {/* <FormField
         label="Name"
         id="name"
         name="name"
@@ -51,7 +68,7 @@ const AddUser = () => {
         onChange={handleInputChange}
         value={formValues.average}
       />
-      <Button type="submit">Add</Button>
+      <Button type="submit">Add</Button> */}
     </ViewWrapper>
   );
 };
