0284cd8a3444f9d7c0297e3d33d6121a032eeb7f
Marcin <marcinmosiejko@gmail.com>
2022-09-24 21:14:26 +0200

    Create basic useMemo example

--- src/views/AddUser.js
+++ src/views/AddUser.js
@@ -1,105 +1,35 @@
-import React, { useContext, useReducer } from 'react';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react';
 import FormField from 'components/molecules/UsersListItem/FormField/FormField';
 import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
 import { Button } from 'components/atoms/Button/Button';
 import { Title } from 'components/atoms/Title/Title';
 import { UsersContext } from 'providers/UsersProvider';
 
-const initialFormState = {
-  name: '',
-  attendance: '',
-  average: '',
-  consent: false,
-  error: '',
-};
+const ChildComponent = ({ someFunction }) => {
+  useEffect(() => {
+    console.log(someFunction());
+  }, [someFunction]);
 
-const actionTypes = {
-  inputChange: 'INPUT CHANGE',
-  clearInputValues: 'CLEAR INPUT VALUES',
-  consentToggle: 'CONSENT TOGGLE',
-  throwError: 'THROW ERROR',
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case actionTypes.inputChange:
-      return { ...state, [action.field]: action.value };
-    case actionTypes.clearInputValues:
-      return initialFormState;
-    case actionTypes.consentToggle:
-      return { ...state, consent: !state.consent };
-    case actionTypes.throwError:
-      return { ...state, error: action.errorMessage };
-    default:
-      return state;
-  }
+  return <p>Hello World</p>;
 };
 
 const AddUser = () => {
-  const [formValues, dispatch] = useReducer(reducer, initialFormState);
-
-  const { handleAddUser } = useContext(UsersContext);
-
-  const handleInputChange = (e) => {
-    dispatch({
-      type: actionTypes.inputChange,
-      field: e.target.name,
-      value: e.target.value,
-    });
-  };
-
-  const handleSubmitUser = (e) => {
-    e.preventDefault();
-
-    if (!formValues.consent) {
-      dispatch({
-        type: actionTypes.throwError,
-        errorMessage: 'You need to give consent',
-      });
-      return;
-    }
-
-    handleAddUser(formValues);
-    dispatch({ type: actionTypes.clearInputValues });
-  };
+  const [value, setValue] = useState(0);
+  const someFunction = useCallback(() => 'Grażyna', []);
 
   return (
-    <ViewWrapper as="form" onSubmit={handleSubmitUser}>
-      <Title>Add new student</Title>
-
-      <FormField
-        label="Name"
-        id="name"
-        name="name"
-        onChange={handleInputChange}
-        value={formValues.name}
-      />
-      <FormField
-        label="Attendance"
-        id="attendance"
-        name="attendance"
-        onChange={handleInputChange}
-        value={formValues.attendance}
-      />
-      <FormField
-        label="Average"
-        id="average"
-        name="average"
-        onChange={handleInputChange}
-        value={formValues.average}
-      />
-      <FormField
-        label="Consent"
-        id="consent"
-        name="consent"
-        type="checkbox"
-        onChange={() => dispatch({ type: actionTypes.consentToggle })}
-        value={formValues.consent}
-      />
-      <Button type="submit">Add</Button>
-      {console.log(formValues.error)}
-      {console.log(formValues.consent)}
-      {formValues.error ? <p>{formValues.error}</p> : null}
+    <ViewWrapper>
+      <ChildComponent someFunction={someFunction} />
+      <Title>Something</Title>
+      <p>{value}</p>
+      <button onClick={() => setValue(value + 1)}>Add</button>
+      <button onClick={() => setValue(value - 1)}>Subtract</button>
     </ViewWrapper>
   );
 };
