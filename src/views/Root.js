ab96400c017aa919d69cb6be363f5314f887a0ed
Marcin <marcinmosiejko@gmail.com>
2022-09-30 17:28:15 +0200

    Add storybook and stories for Button, FormField and StudentsListItem

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from 'assets/styles/globalStyle';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { theme } from 'assets/styles/theme';
 import { Wrapper } from './Root.styles';
 import {
